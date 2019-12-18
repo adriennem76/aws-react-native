5d1618c24258b704932ebd66f8f8a0e73356044f
adriennem76 <adrienne.c.miranda@gmail.com>
2019-12-18 14:48:40 -0500

    wip

--- App.js
+++ App.js
@@ -1,19 +1,108 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import Amplify from 'aws-amplify'
+import config from './aws-exports'
+Amplify.configure(config)
+import { SafeAreaView, StyleSheet, Text, View, TextInput, Button} from 'react-native';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-    </View>
-  );
+import { withAuthenticator } from 'aws-amplify-react-native'
+import { Auth, API, graphqlOperation  } from 'aws-amplify'
+
+import { listRestaurants } from './src/graphql/queries'
+import { createRestaurant } from './src/graphql/mutations'
+
+import uuid from 'uuid/v4'
+const CLIENTID = uuid()
+
+class App extends React.Component {
+  state = {
+    name: '', description: '', city: '', restaurants: []
+  }
+  async componentDidMount() {
+    try {
+      const restaurantData = await API.graphql(graphqlOperation(listRestaurants))
+      console.log('restaurantData:', restaurantData)
+      this.setState({
+        restaurants: restaurantData.data.listRestaurants.items
+      })
+    } catch (err) {
+      console.log('error fetching restaurants...', err)
+    }
+  }
+  createRestaurant = async() => {
+    const { name, description, city  } = this.state
+    // store the restaurant data in a variable
+    const restaurant = {
+      name, description, city, clientId: CLIENTID
+    }
+    // perform an optimistic response to update the UI immediately
+    const restaurants = [...this.state.restaurants, restaurant]
+    this.setState({
+      restaurants,
+      name: '', description: '', city: ''
+      })
+    try {
+      // make the API call
+      await API.graphql(graphqlOperation(createRestaurant, {
+        input: restaurant
+      }))
+      console.log('item created!')
+    } catch (err) {
+      console.log('error creating restaurant...', err)
+    }
+  }
+  // change form state then user types into input
+  onChange = (key, value) => {
+    this.setState({ [key]: value })
+  }
+  // signOut = () => {
+  //   Auth.signOut()
+  //     .then(() => this.props.onStateChange('signedOut'))
+  //     .catch(err => console.log('err: ', err))
+  // }
+  render() {
+    return (
+      <SafeAreaView style={styles.container}>
+        <TextInput
+          style={{ height: 50, margin: 5, backgroundColor: "#ddd" }}
+          onChangeText={v => this.onChange('name', v)}
+          value={this.state.name} placeholder='name'
+        />
+        <TextInput
+          style={{ height: 50, margin: 5, backgroundColor: "#ddd" }}
+          onChangeText={v => this.onChange('description', v)}
+          value={this.state.description} placeholder='description'
+        />
+        <TextInput
+          style={{ height: 50, margin: 5, backgroundColor: "#ddd" }}
+          onChangeText={v => this.onChange('city', v)}
+          value={this.state.city} placeholder='city'
+        />
+        <Button onPress={this.createRestaurant} title='Create Restaurant' />
+        {
+          this.state.restaurants.map((restaurant, index) => (
+            <View key={index} style={styles.item}>
+              <Text style={styles.name}>{restaurant.name}</Text>
+              <Text style={styles.description}>{restaurant.description}</Text>
+              <Text style={styles.city}>{restaurant.city}</Text>
+            </View>
+          ))
+        }
+      </SafeAreaView>
+    )
+  }
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
     justifyContent: 'center',
   },
-});
+  item: { padding: 10 },
+  name: { fontSize: 20 },
+  description: { fontWeight: '600', marginTop: 4, color: 'rgba(0, 0, 0, .5)' },
+  city: { marginTop: 4 }
+})
+
+export default withAuthenticator(App, {
+  includeGreetings: true
+})
---
+++ amplify/backend/function/basiclambda/src/index.js
@@ -0,0 +1,11 @@
+exports.handler = (event, context, callback) => {
+  console.log('event: ', event)
+  const body = {
+    message: "Hello world!"
+  }
+  const response = {
+    statusCode: 200,
+    body
+  }
+  callback(null, response)
+}
