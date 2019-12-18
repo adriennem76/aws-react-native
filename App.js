dcb923b7f2c86dfa65f909108b3d6dff1b003fe9
adriennem76 <adrienne.c.miranda@gmail.com>
2019-12-18 14:56:43 -0500

    coins

--- App.js
+++ App.js
@@ -15,45 +15,20 @@ const CLIENTID = uuid()
 
 class App extends React.Component {
   state = {
-    name: '', description: '', city: '', restaurants: []
+    coins: []
   }
   async componentDidMount() {
     try {
-      const restaurantData = await API.graphql(graphqlOperation(listRestaurants))
-      console.log('restaurantData:', restaurantData)
-      this.setState({
-        restaurants: restaurantData.data.listRestaurants.items
-      })
+      // to get all coins, do not send in a query parameter
+      // const data = await API.get('cryptoapi', '/coins')
+      const data = await API.get('cryptoapi', '/coins?limit=5&start=100')
+      console.log('data from Lambda REST API: ', data)
+      this.setState({ coins: data.coins })
     } catch (err) {
-      console.log('error fetching restaurants...', err)
+      console.log('error fetching data..', err)
     }
   }
-  createRestaurant = async() => {
-    const { name, description, city  } = this.state
-    // store the restaurant data in a variable
-    const restaurant = {
-      name, description, city, clientId: CLIENTID
-    }
-    // perform an optimistic response to update the UI immediately
-    const restaurants = [...this.state.restaurants, restaurant]
-    this.setState({
-      restaurants,
-      name: '', description: '', city: ''
-      })
-    try {
-      // make the API call
-      await API.graphql(graphqlOperation(createRestaurant, {
-        input: restaurant
-      }))
-      console.log('item created!')
-    } catch (err) {
-      console.log('error creating restaurant...', err)
-    }
-  }
-  // change form state then user types into input
-  onChange = (key, value) => {
-    this.setState({ [key]: value })
-  }
+  
   // signOut = () => {
   //   Auth.signOut()
   //     .then(() => this.props.onStateChange('signedOut'))
@@ -61,46 +36,23 @@ class App extends React.Component {
   // }
   render() {
     return (
-      <SafeAreaView style={styles.container}>
-        <TextInput
-          style={{ height: 50, margin: 5, backgroundColor: "#ddd" }}
-          onChangeText={v => this.onChange('name', v)}
-          value={this.state.name} placeholder='name'
-        />
-        <TextInput
-          style={{ height: 50, margin: 5, backgroundColor: "#ddd" }}
-          onChangeText={v => this.onChange('description', v)}
-          value={this.state.description} placeholder='description'
-        />
-        <TextInput
-          style={{ height: 50, margin: 5, backgroundColor: "#ddd" }}
-          onChangeText={v => this.onChange('city', v)}
-          value={this.state.city} placeholder='city'
-        />
-        <Button onPress={this.createRestaurant} title='Create Restaurant' />
+      <View>
         {
-          this.state.restaurants.map((restaurant, index) => (
-            <View key={index} style={styles.item}>
-              <Text style={styles.name}>{restaurant.name}</Text>
-              <Text style={styles.description}>{restaurant.description}</Text>
-              <Text style={styles.city}>{restaurant.city}</Text>
+          this.state.coins.map((c, i) => (
+            <View key={i} style={styles.row}>
+              <Text style={styles.name}>{c.name}</Text>
+              <Text>{c.price_usd}</Text>
             </View>
           ))
         }
-      </SafeAreaView>
+      </View>
     )
   }
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-  },
-  item: { padding: 10 },
-  name: { fontSize: 20 },
-  description: { fontWeight: '600', marginTop: 4, color: 'rgba(0, 0, 0, .5)' },
-  city: { marginTop: 4 }
+  row: { padding: 10 },
+  name: { fontSize: 20, marginBottom: 4 },
 })
 
 export default withAuthenticator(App, {
